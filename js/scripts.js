d5585721b847f111ac8702960d08d81a6cf38f4f
Ryeker <ryekerjh@gmail.com>
2018-04-09 20:59:34 -0700

    worked on vars and alerts

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var myHistory = "It all started in 1986. My mom was a rodie for White Snake and my faither was the lead singer. jkjk. ";
+// console.log(myHistory);
+var myValue = confirm(myHistory);
+console.log(myValue);
