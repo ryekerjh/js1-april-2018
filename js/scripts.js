1d0023f24c67abc0caa10f37288fc20347dc835d
Ryeker <ryekerjh@gmail.com>
2018-04-18 20:04:30 -0700

    stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,28 @@
+/*jshint esversion: 6 */
 
+let answer = prompt('Thank you for participating in my survey.\n\n Please enter your birth date', 'mm/dd/yyyy');
+console.log(answer);
+let birthday = answer !== null ? new Date(answer) : '';
 
-let response = prompt('hey you are the best, tell me stuff');
-
-let secondResponse = prompt('tell me something else');
-
-if(response) {
-  //do more stuff
+if(!birthday) {
+  alert('Oh no! Give me a good date!');
 } else {
-  //Do other stuff
+  var age = (2018 - year);
+  if (age < 18) {
+    console.log(age);
+    console.log('hit less than 18');
+    alert('I am so sorry you are not old enough to participate in this survey.');
+  } else if (age > 79) {
+    alert('I am so sorry you are to old for this survey, but here what was happening  around 80 years ago.\n\nIn 1934, 80 years before 2014, the world was in the Great Depression, Hitler named himself Fuehrer of Germany, Mussolini was Prime Minister of Italy and Roosevelt was President of the United States. The continuing Midwest drought destroyed 35 million acres of farmland in Oklahoma, and President Roosevelt instituted public works programs to build roads, dams and bridges to create jobs. Unemployment was at 22 percent.');
+  } else {
+    let rockBand = prompt('You are the perfect age for my survey!\n\nThe one and only question I for you is\n\nWhats the greatest rock band of all times?');
+    console.log(rockBand.toLowerCase());
+    if (rockBand.toLowerCase() == "led zeppelin", "led zepplin") {
+      alert(`HATS OFF TO YOU MY FRIEND!\n\nLED ZEPPELIN IS THE GREATEST BAND EVER!`);
+      window.location.href = 'http://www.ledzeppelin.com';
+    } else {
+      alert(`YOU MUST BE EXTREMELY DELUSIONAL!\n\n${rockBand.toUpperCase()} IS A MUSICAL JOKE!\n\nLED ZEPPELIN IS THE GREATEST BAND EVER!`);
+      window.location.href = 'http://www.ledzeppelin.com';
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var birthday = new Date('5/21/86');
-// var today = new Date();
-// var timeDiff = Math.abs(today.getTime() - birthday.getTime());
-// var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-// console.log(timeDiff, diffDays/365);
