6701679787a9df6fc983853ac402fa18cadc3684
Ryeker <ryekerjh@gmail.com>
2018-04-18 19:34:41 -0700

    adding stuff

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+
+
+let response = prompt('hey you are the best, tell me stuff');
+
+let secondResponse = prompt('tell me something else');
+
+if(response) {
+  //do more stuff
+} else {
+  //Do other stuff
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var birthday = new Date('5/21/86');
+// var today = new Date();
+// var timeDiff = Math.abs(today.getTime() - birthday.getTime());
+// var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+// console.log(timeDiff, diffDays/365);
