a0a524b734e44a2ff668d7741e37d03d09e90a15
Ryeker <ryekerjh@gmail.com>
2018-04-25 17:07:47 -0700

    day 5 in class

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// console
+//   .log( //This is where the function is called
+//       "hello world" //This is the argument/parameter
+//     ); //Method which is a type of function
+//     //In your browser's console, the RETURN is what gets printed;
+// function greet(type, name, greeting) { //FUNCTION DEFINTION
+  // return `${type} ${name}, ${greeting}!`;
+// }
+
+// let myGreeting = sayHello("Goodbye", "Connie", "You're the greatest!"); //CALL SITE
+
+// alert(myGreeting);
+
+
+
+function sayMyFav() {
+  let fav = "Cheese";
+  function eat() {
+    alert(fav);
+  }
+  eat();
+}
+
+sayMyFav();
+alert(fav);
+
+
+
+
+
+
+
+
+//
