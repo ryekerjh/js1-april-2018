a725c100552e09394fccc7c4b35e75deb649bb2e
Ryeker <ryekerjh@gmail.com>
2018-05-02 18:15:27 -0700

    take home

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,39 @@
-const div = document.querySelector('div');
-const p = document.querySelector('p');
-const button = document.querySelector('.btn-default');
+/*
+1. Ask your user to give you a scale (Celcius or Fahrenheit) and the tempurature they want to convert. So if they give you 90 Fahrenheit, they would want you to convert 90 degrees Fahrenheit to whatever that is in Celcius
+*/
+const h3 = document.querySelector('#temp');
+let scale = prompt("What scale (C or F) do you want to use?", "F or C");
+let temp = prompt("How hot is it?", "99");
+/*
+2. Employ a function that accepts those two values as arguments: temperature and scale (either celcius or fahrenheit) and converts the tempurature they gave you to the opposite scale.
+*/
+function convertTemp(scale, temp) {
+  scale = scale.toLowerCase();
+  let returnVal;
+  let oppositeVal = scale === 'c' ? 'Fahrenheit' : 'Celcius';
+  temp = parseInt(temp);
+  if(!isNaN(temp)) {
+    returnVal = scale === 'c' ? (1.8 * temp) + 32 : (5 / 9) * (temp - 32);
+    /*
+    3. Display the temperature in an h3 with minimal styling.
+    */
+    h3.innerText = `Your temperature is ${returnVal} ${oppositeVal}`;
+  } else {
+    scale = prompt("What scale (C or F) do you want to use?", "F or C");
+    temp = prompt("How hot is it?", "99");
+    convertTemp(scale, temp);
+  }
+}
 
-div.addEventListener('mouseover', (e) => {
-  let newPTag = document.createElement('p'); //Create a new element
-  newPTag.textContent = "Hey there, guy/gal/friend/frienemy!!!"; //add some text content to that element
-  div.appendChild(newPTag); //add that element to another parent element
-});
+convertTemp(scale, temp);
 
-button.addEventListener('click', (e) => {
-  let newDiv = document.createElement('div'); //create a DIV
-  newDiv.innerHTML = '<h3 class="text-center">Whoa! You clicked the thing!</h3><p>Some other things you might be interested in....</p>'; //Add some HTML content to the DIV you made
-  div.insertBefore(newDiv, p); //Add you new DIV to a parent (const div), BEFORE a sibling node (const p).
+/*
+4. From that point on, when the user clicks on the h3, run the function to convert the temperature back and forth between Celcius and Fahrenheit.
+Display the converted temperature in the h3 each time it is changed.
+*/
+h3.addEventListener('click', (e) => {
+  let valueArr = e.target.innerText.split(' ');
+  scale = valueArr[valueArr.length - 1] === 'Celcius' ? 'C' : 'F';
+  temp = valueArr[valueArr.length - 2];
+  convertTemp(scale, temp);
 });
